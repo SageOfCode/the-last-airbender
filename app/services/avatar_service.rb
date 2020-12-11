e8a30418615fccdda4e63df404fddb9a2378454f
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-11 10:02:47 -0700

    Refactor service to follow encapsulation

--- app/services/avatar_service.rb
+++ app/services/avatar_service.rb
@@ -1,9 +1,18 @@
 class AvatarService
   class << self 
     def get_results(search)
-      conn = Faraday.new("https://last-airbender-api.herokuapp.com")
       response = conn.get("/api/v1/characters?affiliation=#{search}")
-      json = JSON.parse(response.body, symbolize_names: true)
+      parse_it(response)
+    end
+
+    private 
+
+    def conn
+      Faraday.new("https://last-airbender-api.herokuapp.com")
+    end
+
+    def parse_it(response)
+      JSON.parse(response.body, symbolize_names: true)
     end
   end
 end
