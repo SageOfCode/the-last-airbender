65322f477d95aaae4717430439fed27f60c4339d
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-11 10:20:23 -0700

    Create search facade spec

---
+++ spec/facades/search_facade_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe SearchFacade do
+  context "class methods" do
+    context ".get_characters" do
+      it "returns park data" do
+        search = SearchFacade.get_characters("fire+nation")
+        expect(search).to be_a(Array)
+        expect(search.count).to eq(20)
+        expect(search.first).to be_a(Character)
+      end
+    end
+  end
+end
