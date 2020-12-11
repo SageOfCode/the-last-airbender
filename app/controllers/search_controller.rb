8ffb935b81d4ee8a66619945273664ebb518aaef
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-11 10:09:10 -0700

    Remove commented out lines from refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,5 @@ class SearchController < ApplicationController
   def index
     nation = params[:nation].sub('_', '+')
     @characters = SearchFacade.get_characters(nation)
-    # conn = Faraday.new("https://last-airbender-api.herokuapp.com")
-    # response = conn.get("/api/v1/characters?affiliation=#{nation}")
-    # json = JSON.parse(response.body, symbolize_names: true)
-    # @characters = json.map do |character|
-    #   Character.new(character)
-    # end
   end
 end
