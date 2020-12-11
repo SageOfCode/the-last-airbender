23f4f5a18b5957ed4c9c7dcd8b599ebcdabb099e
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-11 10:00:08 -0700

    Refactor to get info from facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,12 @@
 class SearchController < ApplicationController
   def index
     nation = params[:nation].sub('_', '+')
-    conn = Faraday.new("https://last-airbender-api.herokuapp.com")
-    response = conn.get("/api/v1/characters?affiliation=#{nation}")
-    json = JSON.parse(response.body, symbolize_names: true)
-    @characters = json.map do |character|
-      Character.new(character)
-    end
+    @characters = SearchFacade.get_characters(nation)
+    # conn = Faraday.new("https://last-airbender-api.herokuapp.com")
+    # response = conn.get("/api/v1/characters?affiliation=#{nation}")
+    # json = JSON.parse(response.body, symbolize_names: true)
+    # @characters = json.map do |character|
+    #   Character.new(character)
+    # end
   end
 end
