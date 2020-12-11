cd6ac6f36949ed4fa4a658f09655f569b0888c3f
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-11 10:53:21 -0700

    Update to character data

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe SearchFacade do
   context "class methods" do
     context ".get_characters" do
-      it "returns park data" do
+      it "returns character data" do
         search = SearchFacade.get_characters("fire+nation")
         expect(search).to be_a(Array)
         expect(search.count).to eq(20)
